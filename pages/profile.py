b79ecd6210fec5a512374bbe11f1908037e8d6c3
Ajay Singh Munda <132570081+AjaySinghMunda@users.noreply.github.com>
2026-07-09 08:36:59 +0530

    Add files via upload

---
+++ pages/profile.py
@@ -0,0 +1,61 @@
+import streamlit as st
+
+import pymongo
+
+conn = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
+mydb = conn["CAP"]
+my = mydb["student"]
+
+@st.dialog("CHANGE PASSWORD")
+def cp():
+    t1 = st.text_input("Enter the old password")
+    t2 = st.text_input("Enter the new password", type="password")
+
+    if st.button("Change Password"):
+
+        my.update_one(
+            {
+                "username": st.session_state["username"],
+                "password": t1
+            },
+            {
+                "$set": {"password": t2}
+            }
+        )
+
+        st.session_state["password"] = t2
+        st.success("Password Changed Successfully")
+        
+        
+        
+        
+#Login check
+if st.session_state.get("username", False):
+    st.write("You are safely inside the app!")
+else:
+       st.warning("First Login !!!")
+       st.stop()
+
+st.balloons()
+c1,c2,c3=st.columns(3)
+st.title("🐍All the basic python code")
+st.success(f"Welcome :{st.session_state['username']}")
+if c1.button("Logout",use_container_width=True):
+       del st.session_state["username"]
+       st.switch_page("main.py")
+if c2.button("See Profile",use_container_width=True):
+       str1=st.session_state["username"]
+       str2=st.session_state["password"]
+       res = my.find({
+        "username": str1,
+        "password": str2
+        })
+
+       for i in res:
+              st.success(f"Email : {i['email']}")
+              st.success(f"Mobile : {i['mobile_number']}")
+              st.success(f"DOB : {i['date_of_birth']}")
+              st.success(f"Password : {i['password']}")
+              
+if c3.button("Change Password",use_container_width=True):
+       cp()
---
+++ pages/signup.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pymongo
+conn=pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")#Set the connection with mongo database and our code
+mydb=conn["CAP"]#CAP is the database name
+my=mydb["student"]#student is a collection on mongodb
+st.title("📝 S i g n U p")
+t1=st.text_input("Username")
+t2=st.text_input("Password",type="password")
+t3=str(st.text_input("Mobile Number"))
+t4=st.text_input("Email Id")
+t5=st.date_input("DOB")
+b1=st.button("SIGNUP")
+if b1:
+       my.insert_one({
+        "username": t1,
+        "email": t4,
+        "mobile_number": t3,
+        "date_of_birth": str(t5),
+        "password": t2
+        })
+      
+       st.success("✅ Sign Up Successfully")
