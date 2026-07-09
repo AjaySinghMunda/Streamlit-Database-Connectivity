043fd48760bf3b4b3882a77a1e989b1c70edf474
Ajay Singh Munda <132570081+AjaySinghMunda@users.noreply.github.com>
2026-07-09 11:26:55 +0530

    Update signup.py

--- pages/signup.py
+++ pages/signup.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pymongo
-conn=pymongo.MongoClient("mongodb+srv://ajay08singhmunda_db_user:PKQROAajnfpfBQOB@cluster0.whgecul.mongodb.net/?appName=Cluster0")#Set the connection with mongo database and our code
+conn=pymongo.MongoClient("mongodb+srv://ajay08singhmunda_db_user:Ajay08MongoDB@cluster0.whgecul.mongodb.net/?appName=Cluster0")#Set the connection with mongo database and our code
 mydb=conn["CAP"]#CAP is the database name
 my=mydb["student"]#student is a collection on mongodb
 st.title("📝 S i g n U p")
