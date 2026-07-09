5af01e858e6b92ab78406a01d2b381249b587459
Ajay Singh Munda <132570081+AjaySinghMunda@users.noreply.github.com>
2026-07-09 11:18:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import streamlit as st 
+import streamlit as st
+import pymongo
 
+conn = pymongo.MongoClient(
+    st.secrets["MONGO_URI"],
+    serverSelectionTimeoutMS=5000
+)
+
+try:
+    conn.admin.command("ping")
+    st.success("Connected to MongoDB Atlas")
+except Exception as e:
+    st.error(e)
