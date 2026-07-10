483bc8d70b51bc36f3b05be36517521d2a6ae7f9
Ajay <132570081+Ajayjsb@users.noreply.github.com>
2026-07-10 07:42:06 +0530

    Workflow: main

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# import streamlit as st
-# import pymongo
-
-# conn = pymongo.MongoClient(st.secrets["MONGO_URI"])
-
-# try:
-#     conn.admin.command("ping")
-#     st.success("Connected to MongoDB Atlas")
-# except Exception as e:
-#     st.error(e)
-
-
-
 import streamlit as st
+import pymongo
+
+conn = pymongo.MongoClient(st.secrets["MONGO_URI"])
+
+try:
+    conn.admin.command("ping")
+    st.success("Connected to MongoDB Atlas")
+except Exception as e:
+    st.error(e)
+
+
+
+# import streamlit as st
 
 # --------------------------------------------------
 # Page Configuration
